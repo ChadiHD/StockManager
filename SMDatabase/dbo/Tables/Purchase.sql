c2f5ec10c34429364a02118e86e2a1f82149ed8c
ChadiHD <rollmon112@gmail.com>
2023-02-18 17:44:58 +0000

    Register Database Tables
    Tables for basic Registration system

---
+++ SMDatabase/dbo/Tables/Purchase.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Purchase]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [StaffId] NVARCHAR(128) NOT NULL, 
+    [PurchaseDate] DATETIME2 NOT NULL , 
+    [SubTotal] MONEY NOT NULL, 
+    [VAT] MONEY NOT NULL, 
+    [FinalPrice] MONEY NOT NULL
+)
---
+++ SMDatabase/dbo/Tables/PurchaseDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[PurchaseDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [PurchaseId] INT NOT NULL, 
+    [ProductId] INT NOT NULL, 
+    [Quantity] INT NOT NULL DEFAULT 1,
+    [PurchasePrice] MONEY NOT NULL, 
+    [VAT] MONEY NOT NULL DEFAULT 0, 
+)
