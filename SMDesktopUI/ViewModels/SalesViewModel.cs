e17b782ad75dc6dc1c1f1e7a882a5745fe210ebd
ChadiHD <rollmon112@gmail.com>
2023-03-01 00:36:55 +0000

    Wire Up the WPF Shopping Cart
    Modifing the view models to display the items in their text boxes, and get the subtotal, and update quantities

--- SMDesktopUI/ViewModels/SalesViewModel.cs
+++ SMDesktopUI/ViewModels/SalesViewModel.cs
@@ -45,9 +45,23 @@ namespace SMDesktopUI.ViewModels
             }
         }
 
-        private BindingList<ProductModel> _cart;
+        private ProductModel _selectedProduct;
 
-        public BindingList<ProductModel> Cart
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set 
+            { 
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
@@ -57,7 +71,7 @@ namespace SMDesktopUI.ViewModels
             }
         }
 
-        private int _itemQuantity;
+        private int _itemQuantity = 1;
 
         public int ItemQuantity
         {
@@ -66,6 +80,7 @@ namespace SMDesktopUI.ViewModels
             {
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
@@ -73,8 +88,13 @@ namespace SMDesktopUI.ViewModels
         {
             get
             {
-                // TODO - Create the calculations
-                return "£0.00";
+                decimal subTotal = 0;
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+
+                return subTotal.ToString("C");
             }
         }
 
@@ -103,8 +123,11 @@ namespace SMDesktopUI.ViewModels
                 bool output = false;
                 
                 // Validate if something is selected 
-
-                // Validate of there is an item in cart
+                // Validate if there is an item in cart
+                if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
 
                 return output;
             }
@@ -112,7 +135,27 @@ namespace SMDesktopUI.ViewModels
 
         public void AddToCart()
         {
-
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                // Refreshing the cart list with new added items
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel()
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(item);
+            }
+            
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanRemoveFromCart
@@ -129,7 +172,7 @@ namespace SMDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
-
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanCheckOut
