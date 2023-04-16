80a877e027eb93afa4a4ef49b8a07b8790db9051
ChadiHD <rollmon112@gmail.com>
2023-04-16 22:17:34 +0100

    Add JWT Authentication
    Created classes for JWT Authentication for Blazor Webassembly app

--- SMDesktopUI.Library/Api/APIHelper.cs
+++ SMDesktopUI.Library/Api/APIHelper.cs
@@ -1,4 +1,5 @@
-﻿using SMDesktopUI.Library.Models;
+﻿using Microsoft.Extensions.Configuration;
+using SMDesktopUI.Library.Models;
 using SMDesktopUI.Models;
 using System;
 using System.Collections.Generic;
@@ -14,11 +15,13 @@ namespace SMDesktopUI.Library.Api
         // HttpClient is instantiated once for as long as runtime
         private HttpClient _apiClient;
         private ILoggedInUserModel _loggedInUser;
+        private readonly IConfiguration _config;
 
-        public APIHelper(ILoggedInUserModel loggedInUser)
+        public APIHelper(ILoggedInUserModel loggedInUser, IConfiguration config)
         {
-            InitializeClient();
             _loggedInUser = loggedInUser;
+            _config = config;
+            InitializeClient();
         }
 
         public HttpClient ApiClient
@@ -45,7 +48,7 @@ namespace SMDesktopUI.Library.Api
                 new KeyValuePair<string, string>("username", username),
                 new KeyValuePair<string, string>("password", password)
             });
-            using (HttpResponseMessage response = await _apiClient.PostAsync(_apiClient.BaseAddress + "Token", data))
+            using (HttpResponseMessage response = await _apiClient.PostAsync("/Token", data))
             {
                 if (response.IsSuccessStatusCode)
                 {
@@ -72,12 +75,12 @@ namespace SMDesktopUI.Library.Api
             _apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             _apiClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
 
-            using (HttpResponseMessage response = await _apiClient.GetAsync(_apiClient.BaseAddress + "api/User"))
+            using (HttpResponseMessage response = await _apiClient.GetAsync("/api/User"))
             {
                 if (response.IsSuccessStatusCode )
                 {
                     // Map the data between the interface and database
-                    var results = await response.Content.ReadAsStringAsync<LoggedInUserModel>();
+                    var results = await response.Content.ReadAsAsync<LoggedInUserModel>();
                     _loggedInUser.CreatedDate = results.CreatedDate;
                     _loggedInUser.EmailAddress = results.EmailAddress;
                     _loggedInUser.FirstName = results.FirstName;
---
+++ SMPortal/Authentication/AuthStateProvider.cs
@@ -0,0 +1,53 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
+
+namespace SMPortal.Authentication
+{
+    public class AuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorge;
+        private readonly AuthenticationState _anonymous;
+
+        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorge)
+        {
+            _httpClient = httpClient;
+            _localStorge = localStorge;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _localStorge.GetItemAsync<string>("authToken");
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return _anonymous;
+            }
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+
+            return new AuthenticationState(
+                new ClaimsPrincipal(
+                    new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token),
+                            "jwtAuthType")));
+        }
+
+        public void MarkUserAsAuthenticated(string token)
+        {
+            var authenticatedUser = new ClaimsPrincipal(
+                new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token),
+                    "jwtAuthType"));
+            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        public void MarkUserAsLoggedOut()
+        {
+            var authState = Task.FromResult(_anonymous);
+            NotifyAuthenticationStateChanged(authState);
+        }
+    }
+}
